3391882c1a1a7a60bd88526d53325f5099650f75
grita8 <chajiazineb68@gmail.com>
2026-02-02 16:11:42 +0000

    done

---
+++ push_swap.h
@@ -0,0 +1,78 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include "libft.h"
+#include <stdlib.h>
+
+typedef struct stack_list
+{
+    int             content;
+    struct stack_list   *next;
+} list_t;
+
+typedef struct s_state
+{
+    int i;
+    int start;
+    int k;
+    int cw;
+    int end;
+    char **tow_d;
+}       t_state; 
+
+char	*join_args(int argc, char **argv);
+char *ft_sstrjoin(char *save, char *buff);
+char	*ftt_free(char **arr);
+void initialise(t_state *state, char *args);
+char *ft_strcopy(char *str, int start, int end);
+char **split_args(char *args);
+int	count_word(char *args);
+list_t *creat_stack_from_split(char **tow_d);
+
+void	is_it_null(char **argv, int argc);
+list_t  *new_node(int content);
+void    add_back(list_t **stack, list_t *new);
+list_t  *creat_stack(int argc, char **argv);
+void    ft_putnbr(int nb);
+void    free_stack(list_t *stack);
+
+int	swap_stack_b(list_t **stack_b);
+int	swap_stack_a(list_t **stack_a);
+void ft_putstr(char *str);
+
+
+void push_min_to_b(list_t **stack_a, list_t **stack_b);
+int min_position(list_t **stack);
+int stack_size(list_t *stack);
+void sort2(list_t **stack_a);
+void sort3(list_t **stack_a);
+void sort4(list_t **stack_a, list_t **stack_b);
+void sort5(list_t **stack_a, list_t **stack_b);
+int sorted(list_t **stack_a);
+
+int	sa(list_t **stack_b);
+int	sb(list_t **stack_b);
+int ss(list_t **stack_a, list_t **stack_b, int index);
+
+int push_a(list_t **stack_a, list_t **stack_b);
+int push_b(list_t **stack_a, list_t **stack_b);
+
+int pb(list_t **stack_a, list_t **stack_b);
+int pa(list_t **stack_a, list_t **stack_b);
+
+int rotate_stack_a(list_t **stack_b);
+int rotate_stack_b(list_t **stack_b);
+
+int	ra(list_t **stack_a);
+int	rb(list_t **stack_a);
+int rr(list_t **stack_a, list_t **stack_b, int index);
+
+int reverse_rotate_stack_a(list_t **stack_a);
+int reverse_rotate_stack_b(list_t **stack_b);
+
+int	rra(list_t **stack_a);
+int	rrb(list_t **stack_a);
+int rrr(list_t **stack_a, list_t **stack_b, int index);
+
+
+#endif
